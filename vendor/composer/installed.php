a236978f6a638cfa33a8ee13fba396f5c103903b
hevarn <nunzio.moderni@gmail.com>
2021-11-05 18:17:20 +0100

    creation d'un form securiser en JS et PHP

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4babe786bd405e62361df800f1869000005e800f',
+        'name' => 'hevarne/projet-contact-us-php',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hevarne/projet-contact-us-php' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4babe786bd405e62361df800f1869000005e800f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
